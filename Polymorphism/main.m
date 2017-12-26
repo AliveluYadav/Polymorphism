6d94a0df04aaee29ff75be2d626818d40a946777
Alivelu Ravula <alivelu7193@achiever-13.local>
2017-12-26 15:04:03 +0530

    Initial Commit

---
+++ Polymorphism/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Polymorphism
+//
+//  Created by Alivelu Ravula on 12/26/17.
+//  Copyright © 2017 Alivelu Ravula. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
